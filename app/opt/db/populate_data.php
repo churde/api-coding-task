40ba4d4c39b8944b1f31b85f32733aaa3a082d83
Xurde McFly <churde@gmail.com>
2024-10-15 06:06:26 +0200

    Improve birth date faker data based on the character race (just for fun)

--- app/opt/db/populate_data.php
+++ app/opt/db/populate_data.php
@@ -34,8 +34,37 @@ $lotrFactions = ['Elves', 'Men', 'Dwarves', 'Hobbits', 'Orcs', 'Uruk-hai', 'Ents
 $lotrEquipmentTypes = ['Sword', 'Bow', 'Axe', 'Staff', 'Armor', 'Shield', 'Ring'];
 $lotrKingdoms = ['Gondor', 'Rohan', 'Rivendell', 'Lothlorien', 'Erebor', 'Moria', 'The Shire', 'Mordor', 'Isengard'];
 
+// Function to generate appropriate birth date based on race
+function generateBirthDate($race) {
+    $currentYear = 3019; // Year of the War of the Ring
+    $minYear = 1; // Minimum year for MySQL DATE type
+
+    switch ($race) {
+        case 'Elves':
+            $year = $currentYear - rand(100, 3018);
+            break;
+        case 'Men':
+        case 'Hobbits':
+            $year = $currentYear - rand(20, 120);
+            break;
+        case 'Dwarves':
+            $year = $currentYear - rand(20, 250);
+            break;
+        case 'Wizards':
+            $year = $minYear;
+            break;
+        default:
+            $year = $currentYear - rand(1, 200);
+    }
+
+    $month = str_pad(rand(1, 12), 2, '0', STR_PAD_LEFT);
+    $day = str_pad(rand(1, 28), 2, '0', STR_PAD_LEFT); // Using 28 to avoid issues with February
+
+    return "$year-$month-$day";
+}
+
 // Populate Factions
-for ($i = 1; $i <= 100; $i++) {
+for ($i = 1; $i <= 43; $i++) {
     $stmt = $pdo->prepare("INSERT INTO factions (faction_name, description) VALUES (:faction_name, :description)");
     $stmt->execute([
         'faction_name' => $faker->randomElement($lotrFactions),
@@ -44,7 +73,7 @@ for ($i = 1; $i <= 100; $i++) {
 }
 
 // Populate Equipments
-for ($i = 1; $i <= 100; $i++) {
+for ($i = 1; $i <= 86; $i++) {
     $stmt = $pdo->prepare("INSERT INTO equipments (name, type, made_by) VALUES (:name, :type, :made_by)");
     $stmt->execute([
         'name' => $faker->word . ' of ' . $faker->word,
@@ -55,10 +84,13 @@ for ($i = 1; $i <= 100; $i++) {
 
 // Populate Characters
 for ($i = 1; $i <= 100; $i++) {
+    $race = $faker->randomElement($lotrFactions);
+    $birthDate = generateBirthDate($race);
+
     $stmt = $pdo->prepare("INSERT INTO characters (name, birth_date, kingdom, equipment_id, faction_id) VALUES (:name, :birth_date, :kingdom, :equipment_id, :faction_id)");
     $stmt->execute([
         'name' => $faker->firstName . ' ' . $faker->lastName,
-        'birth_date' => $faker->dateTimeBetween('-100 years', 'now')->format('Y-m-d'),
+        'birth_date' => $birthDate,
         'kingdom' => $faker->randomElement($lotrKingdoms),
         'equipment_id' => rand(1, 100),
         'faction_id' => rand(1, 100),
